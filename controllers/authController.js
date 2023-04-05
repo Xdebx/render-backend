f7ce868c07741f8afe8e2375265c85879f89b5d6
Xdebx <86518562+Xdebx@users.noreply.github.com>
2023-04-05 13:53:49 +0800

    logouuuuuuut

--- controllers/authController.js
+++ controllers/authController.js
@@ -131,6 +131,8 @@ exports.logout = async (req, res, next) => {
         res.cookie("token", null, {
             expires: new Date(Date.now()), // Set the expiration date to a past date
             httpOnly: true, // Set the cookie as httpOnly to prevent client-side access
+            secure: true,
+            sameSite: 'none'
         });
 
         // Perform additional actions to fully log out the user on the server-side
